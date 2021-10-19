36476c4bc5ef045cca6c1558e507ccb2b4e1bc9c
Daqingtian123 <88302194+Daqingtian123@users.noreply.github.com>
2021-10-19 21:33:32 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace my_first_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("打个招呼");
+            Console.ReadKey();
+            string str = Console.ReadLine();
+            Console.WriteLine("{0}",str);
+            Console.ReadKey();
+
+
+        }
+    }
+}
